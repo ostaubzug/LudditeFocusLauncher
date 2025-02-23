903ef1123eb40f2121829c07527f69ce128e20ed
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2025-02-23 11:17:37 +0100

    added tailwind to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,8 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'LudditeLauncher';
